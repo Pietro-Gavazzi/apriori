b5b4f8d4c81ef52f4cd79f3b64a212f0a3374508
Mirco2002 <163852533+Mirco2002@users.noreply.github.com>
2024-04-04 10:28:04 +0200

    Update template_mirco.py
    Last update

--- linfo2364_project2/template_mirco.py
+++ linfo2364_project2/template_mirco.py
@@ -1,19 +1,29 @@
 import sys
 from sklearn import tree, metrics
 import numpy as np
+from copy import copy
 
 
 class Spade:
     def __init__(self, pos_filepath, neg_filepath, k):
         self.pos_transactions = get_transactions(pos_filepath)
         self.neg_transactions = get_transactions(neg_filepath)
-        self.pos_patterns_in_trans = {}  # {pattern : [0, 1, 0, 1, 1, 0, 1, 0, 1, 1]} where 1 means the pattern is present in the transaction
+        self.pos_patterns_in_trans = {}  # {pattern1 : {0,3,11,12,15,20, ...}} where we have the set of the transactions that contain each pattern
         self.neg_patterns_in_trans = {}
         self.k = k
 
     # Feel free to add parameters to this method
-    def mine_top_k(self):
+    def mine_top_k(self, metric, min_support):
+        """
+        Mine the top-k best patterns from the positive and negative transactions.
+        :param metric: metric to be used to evaluate the patterns
+        :param min_support: minimum support threshold
+        :return: Top-k best patterns w.r.t the metric
+        """
+
+        # TODO: Implement this method
         pass
+
 
     def convert_dict_to_np_array(self, patterns_in_trans):
         """
@@ -82,9 +92,10 @@ class Spade:
             neg_train_set_ids = [i for i in range(len(self.neg_transactions)) if
                                  i < fold * neg_fold_size or i >= (fold + 1) * neg_fold_size]
 
-            self.mine_top_k()  # The self.pos_patterns_in_trans (and self.neg_patterns_in_trans) will be updated here
+            self.mine_top_k(None, None)  # The self.pos_patterns_in_trans (and self.neg_patterns_in_trans) will be updated here
 
             m = self.get_feature_matrices(pos_train_set_ids, neg_train_set_ids)
+
             classifier = tree.DecisionTreeClassifier(random_state=1)
             classifier.fit(m['train_matrix'], m['train_labels'])
 
@@ -94,37 +105,96 @@ class Spade:
 
 
 def get_transactions(filepath):
-    transactions = []
+    transactions = {}
+    index = 0
     with open(filepath) as f:
         new_transaction = True
         for line in f:
             if line.strip():
                 if new_transaction:
-                    transactions.append([])
                     new_transaction = False
+                    transactions[index] = []  # Initialize new transaction list
                 element = line.split(" ")
-                assert (int(element[1]) - 1 == len(transactions[-1]))
-                transactions[-1].append(element[0])
+                assert (int(element[1]) - 1 == len(transactions[index]))
+                transactions[index].append(element[0])
             else:
                 new_transaction = True
+                index += 1
     return transactions
 
 
 def spade_repr_from_transaction(transactions):
     spade_repr = {}
     covers = {}
-    for tid, transaction in enumerate(transactions):
-        for i, item in enumerate(transaction):
+    for tid, transaction in transactions.items():  # Iterate over each transaction
+        for i, item in enumerate(transaction):  # Iterate over each item in the transaction
             try:
-                covers[item].add(tid)
+                covers[item].add(tid)  # Add transaction id to the set of transactions covering the item
             except KeyError:
-                covers[item] = {tid}
+                covers[item] = {tid}  # Create a new set if the item is not in covers
             try:
-                spade_repr[item].append((tid, i))
+                spade_repr[item].append((tid, i))  # Add the (transaction id, position) tuple to spade_repr
             except KeyError:
-                spade_repr[item] = [(tid, i)]
+                spade_repr[item] = [(tid, i)]  # Create a new list if the item is not in spade_repr
     return {'repr': spade_repr, 'covers': covers}
-    return projected, cover
+
+
+
+#================================================================================================
+# Sequential Covering Algorithm
+#================================================================================================
+
+
+
+def remove_covered_transactions(dataset : Spade, pattern):
+    """
+    Remove the transactions covered by the pattern
+    :param dataset: Spade object
+    :param pattern: pattern
+    :return: list of transactions
+    """
+    remaining_transactions = copy(dataset)
+
+    # Remove the transactions covered by the pattern
+    pos_trans_containing_pattern = remaining_transactions.pos_patterns_in_trans.pop(pattern)
+    for trans in pos_trans_containing_pattern:
+        remaining_transactions.pos_transactions.pop(trans)
+
+    neg_trans_containing_pattern = remaining_transactions.neg_patterns_in_trans.pop(pattern)
+    for trans in neg_trans_containing_pattern:
+        remaining_transactions.neg_transactions.pop(trans)
+
+    return remaining_transactions
+
+
+
+def SequentialCoveringAlgorithm(dataset, k, metric, min_support):
+    """
+    Sequential Covering Algorithm
+    :param dataset: list of transactions
+    :param k: number of patterns to be mined
+    :param metric: metric to be used to evaluate the patterns
+    :param min_support: minimum support threshold
+    :return: list of patterns
+    """
+    remaining_transactions = copy(dataset)
+
+    patterns = {}
+    for i in range(k):
+        # Find the best pattern
+        best_pattern = remaining_transactions.mine_top_k(remaining_transactions, metric, min_support, k=1)
+        patterns.add(best_pattern)
+        # Remove the transactions covered by the best pattern
+        remaining_transactions = remove_covered_transactions(dataset, best_pattern)
+    return patterns
+
+
+
+#================================================================================================
+# Main
+#================================================================================================
+
+
 
 
 if __name__ == '__main__':
@@ -138,6 +208,15 @@ if __name__ == '__main__':
     # Update manually the variable self.pos_patterns_in_trans taking into account only single symbols
     s.pos_patterns_in_trans = spade_repr_from_transaction(s.pos_transactions)['covers']
     s.neg_patterns_in_trans = spade_repr_from_transaction(s.neg_transactions)['covers']
+
+    print(s.pos_transactions)
+    print(s.pos_patterns_in_trans)
+
+    #s = remove_covered_transactions(s, 'W')
+    #print(s.pos_transactions)
+
+
+
 
     # Perform cross-validation with 5 folds
     s.cross_validation(nfolds=5)
