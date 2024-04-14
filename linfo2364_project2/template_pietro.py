f58bbbf7d876576993ab8821c79e2991ca0827c2
pgavazzi <pietro.gavazzi@student.uclouvain.be>
2024-04-14 13:13:08 +0200

    working on wracc

--- linfo2364_project2/template_pietro.py
+++ linfo2364_project2/template_pietro.py
@@ -1,6 +1,11 @@
 import sys
 import heapq
 import numpy as np
+from copy import copy
+
+import pandas as pd
+from sklearn import tree, metrics
+
 
 
 # sys.setrecursionlimit(sys.maxunicode)
@@ -19,90 +24,42 @@ class Spade:
         self.neg_transactions_cover = self.neg_transactions['covers']
 
         self.k = k
-        
+        self.top_k_patterns = None
 
+      
 
-    # Feel free to add parameters to this method
     def min_top_k(self):
-
         k=self.k
-
         D = self.pos_transactions_repr.copy()
-        # print(D)
+
         for j, transaction in self.neg_transactions_repr.items():
             try:
                 D[j].update(transaction)
             except:
                 D[j] = transaction
 
-        # print(D)
-
         frequent_sequences = []
         P = {}
-
 
         heapq.heapify(frequent_sequences)
         min_support = 1
 
-        for s in D:
-            heapq.heappush(frequent_sequences, (len(D[s]),s,D[s]))
-            P[s] = D[s] 
-            # print(frequent_sequences)
-            # print(min_support)
-
-        nb_exces_sequences = len(frequent_sequences)-k if len(frequent_sequences)-k > 0 else 0
-
-        # print(P)
-        # print(nb_exces_sequences)
-
-        while frequent_sequences[nb_exces_sequences-1][0] == frequent_sequences[nb_exces_sequences][0]:
-            nb_exces_sequences -=1
-            if nb_exces_sequences < 0:
-                break
-
-        while nb_exces_sequences > 0:
-            unfrequent = heapq.heappop(frequent_sequences)
-            if unfrequent[1] in P:
-                P.pop(unfrequent[1])
-            nb_exces_sequences-=1
-        
-            min_support = frequent_sequences[0][0]
-
-        # print(frequent_sequences)
-
-        get_frequent_sequences(P, k, min_support, frequent_sequences)
-
-        elements = [heapq.heappop(frequent_sequences) for j in range(len(frequent_sequences)) ]
-        return ({j[1]:set(j[2].keys()) for j in elements}, self.P) # -> return un tuple, le premier element ce sera tj {"nom_pattern":{transactions_id}}, le deuxième élément ce sera nb transaction positive 
+        for sequence in D:
+            support = len(D[sequence])
+            heapq.heappush(frequent_sequences, (support, sequence, D[sequence]))
+            P[sequence] = D[sequence]
 
 
-    
-    def get_feature_matrices(self):
-        return {
-            'train_matrix': [],
-            'test_matrix': [],
-            'train_labels': [],
-            'test_labels': [],
-        }
-    
+        not_frequent_anymore = set()
+        remove_unfrequent(k, frequent_sequences, not_frequent_anymore)
+        get_frequent_sequences(P, k, min_support, frequent_sequences, not_frequent_anymore)
 
-    def cross_validation(self, nfolds):
-        pos_fold_size = len(self.pos_transactions) // nfolds
-        neg_fold_size = len(self.neg_transactions) // nfolds
-        for fold in range(nfolds):
-            print('fold {}'.format(fold + 1))
-            pos_train_set = {i for i in range(len(self.pos_transactions)) if i < fold*pos_fold_size or i >= (fold+1)*pos_fold_size}
-            neg_train_set = {i for i in range(len(self.neg_transactions)) if i < fold*neg_fold_size or i >= (fold+1)*neg_fold_size}
+        elements = [heapq.heappop(frequent_sequences) for j in range(len(frequent_sequences))]
 
-            self.mine_top_k()
-            
-            m = self.get_feature_matrices()
-            classifier = tree.DecisionTreeClassifier(random_state=1)
-            classifier.fit(m['train_matrix'], m['train_labels'])
+        # -> return un tuple, le premier element ce sera tj {"nom_pattern":{transactions_id}}, le deuxième élément ce sera nb transaction positive
+        return ({j[1]:set(j[2].keys()) for j in elements}, self.P) 
 
-            predicted = classifier.predict(m['test_matrix'])
-            accuracy = metrics.accuracy_score(m['test_labels'], predicted)
-            print(f'Accuracy: {accuracy}')
+
 
 def get_transactions(filepath):
     transactions = []
@@ -119,6 +76,7 @@ def get_transactions(filepath):
             else:
                 new_transaction = True
     return transactions
+
 
 def spade_repr_from_transaction(transactions, min_id=0):
     spade_repr = {}
@@ -142,50 +100,118 @@ def spade_repr_from_transaction(transactions, min_id=0):
 
 
 
-def get_frequent_sequences(P, top_k, min_support, frequent_sequences):
-    # print(len(frequent_sequences))
-    # print(P)
-    unfrequent_P = set()
+
+def remove_unfrequent(k:int, list_best_sequences:list, list_not_best_anymore:set):
+
+    # calculate how many sequences are in excess, if there are not we don't have unfrequent
+    nb_exces_sequences = len(list_best_sequences)-k if len(list_best_sequences)-k > 0 else 0
+    if nb_exces_sequences == 0: return
+
+
+    # take n+1 best elements, and remove elements with value strictly inferior to the last best sequence, update "not_best_anymore"
+    excess_smallest_sequences = heapq.nsmallest(nb_exces_sequences+1, list_best_sequences)
+
+    while excess_smallest_sequences[nb_exces_sequences][0] == excess_smallest_sequences[nb_exces_sequences-1][0]:
+        nb_exces_sequences -=1
+        if nb_exces_sequences < 0:
+            break
+
+    while nb_exces_sequences > 0:
+        not_best_sequence_anymore = heapq.heappop(list_best_sequences)
+        list_not_best_anymore.add(not_best_sequence_anymore[1])
+        nb_exces_sequences-=1
+
+
+
+
+def get_frequent_sequences(P, top_k, min_support, frequent_sequences,  not_frequent_anymore):
+
     for ra in P:
-        if ra in unfrequent_P:
+        if ra in not_frequent_anymore:
             continue
         Pa = {}
-        if len(frequent_sequences)<k:
-            min_support = 1
-        else:
-            min_support = heapq.nsmallest(1, frequent_sequences)[0][0]
         for rb in P:
-            if rb in unfrequent_P:
-                continue 
+            if rb in not_frequent_anymore:
+                continue
 
             rab, P_rab = intersect(ra, rb, P)
 
-            if len(P_rab)>=min_support: 
+            if len(P_rab)>=min_support:
                 heapq.heappush(frequent_sequences, (len(P_rab),rab,P_rab))
-                Pa[rab] = P_rab 
+                Pa[rab] = P_rab
         if Pa:
-            nb_exces_sequences = len(frequent_sequences)-k if len(frequent_sequences)-k > 0 else 0
+            # remove elements that became unfrequent
+            remove_unfrequent(k, frequent_sequences, not_frequent_anymore)
 
-            n_smallest = heapq.nsmallest(nb_exces_sequences+1, frequent_sequences)
+            # update min_support after removind unfrequent
+            if len(frequent_sequences)<k:
+                min_support = 1
+            else:
+                min_support = heapq.nsmallest(1, frequent_sequences)[0][0]
 
-            while n_smallest[nb_exces_sequences-1][0] == n_smallest[nb_exces_sequences][0]:
-                nb_exces_sequences -=1
-                if nb_exces_sequences < 0:
-                    break
-            while nb_exces_sequences > 0:
-                unfrequent = heapq.heappop(frequent_sequences)
-                if unfrequent[1] in Pa:
-                    Pa.pop(unfrequent[1])
-                if unfrequent[1] in P:
-                    unfrequent_P.add(unfrequent[1])
-                nb_exces_sequences-=1
+            if Pa: 
+                get_frequent_sequences(Pa, top_k, min_support, frequent_sequences, not_frequent_anymore)
+                # update min_support after recursive call
+                if len(frequent_sequences)<k:
+                    min_support = 1
+                else:
+                    min_support = heapq.nsmallest(1, frequent_sequences)[0][0]
 
-            if Pa: get_frequent_sequences(Pa, top_k, min_support, frequent_sequences)
     return frequent_sequences
 
 
+
+
+
+
+
+
+def get_best_wrack_sequences(P, top_k, nb_pos, nb_neg, min_positive_support, frequent_sequences, not_frequent_anymore):
+
+    for ra in P:
+        if ra in not_frequent_anymore:
+            continue
+        Pa = {}
+        for rb in P:
+            if rb in not_frequent_anymore:
+                continue
+
+            rab, P_rab = intersect(ra, rb, P)
+
+            if len(P_rab)>=1:
+                pos_support = get_positive_support(nb_pos, P_rab)
+                if pos_support >= min_positive_support:
+                    Wracc = weighted_relative_accuracy(nb_pos, nb_neg, P_rab)
+                    heapq.heappush(frequent_sequences, (Wracc,rab,P_rab))
+                    Pa[rab] = P_rab
+        if Pa:
+            # remove elements that became unfrequent
+            remove_unfrequent(k, frequent_sequences, not_frequent_anymore)
+
+            # update min_support after removind unfrequent
+            if len(frequent_sequences)<k:
+                min_positive_support = 1
+            else:
+                min_positive_support = heapq.nsmallest(1, frequent_sequences)[0][0]
+
+            if Pa: 
+                get_best_wrack_sequences(Pa, top_k, min_positive_support, frequent_sequences, not_frequent_anymore)
+                # update min_support after recursive call
+                if len(frequent_sequences)<k:
+                    min_positive_support = 1
+                else:
+                    min_positive_support = heapq.nsmallest(1, frequent_sequences)[0][0]
+
+    return frequent_sequences
+
+
+
+
+
+
 def intersect(ra, rb, P):
-    rab = ra+rb[-1]
+    rab = ra + "-" + rb.split("-")[-1]
+    #rab = ra+rb[-1]
     transaction_in_common_ids = P[ra].keys()&P[rb].keys()
     P_rab = {}
     for t_id in transaction_in_common_ids:
@@ -193,29 +219,40 @@ def intersect(ra, rb, P):
         position_list_ab = [pos_b for pos_b in P[rb][t_id] if pos_b>pos_a]
         if position_list_ab:
             P_rab[t_id] = position_list_ab
+
     return rab, P_rab
 
 
-def weighted_relative_accuracy(P, N, x):
-    p = sum([i <= P for i in x.keys()])
-    n = len(x)-p 
+def get_min_positive_support(min_Wracc, nb_pos, nb_neg):
+    return (((nb_pos+nb_neg)**2)/nb_neg)*min_Wracc
+
+
+def get_positive_support(P, transactions_containing_pattern):
+    return len([i < P for i in transactions_containing_pattern])
+
+def weighted_relative_accuracy(nb_pos, nb_neg, transactions_containing_pattern):
+    p = sum([i < nb_pos for i in transactions_containing_pattern])
+    n = len(transactions_containing_pattern)-p
     # print(P)
     # print(N)
-    return (P/(P+N))*(N/(P+N))*(p/P-n/N)
+    return (nb_pos/(nb_pos+nb_neg))*(nb_neg/(nb_pos+nb_neg))*(p/nb_pos-n/nb_neg)
 
 
 if __name__ == '__main__':
+    import timeit
     pos_filepath = "datasets/Protein/PKA_group15.txt"
     neg_filepath = "datasets/Protein/SRC1521.txt"
 
-    pos_filepath = "Test/positive.txt"
-    neg_filepath = "Test/negative.txt"
+    # pos_filepath = "Test/positive.txt"
+    # neg_filepath = "Test/negative.txt"
 
     # Create the object
-    k = 32
+    k = 100
+    a = timeit.default_timer()
     s = Spade(pos_filepath, neg_filepath, k)
-
     sol = s.min_top_k()
+    b = timeit.default_timer()
+    print(b-a)
 
 
 
